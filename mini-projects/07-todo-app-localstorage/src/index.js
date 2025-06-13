ca36c1689340b3bac446f8f1eea6760c85fd6c60
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-06-13 12:45:50 +0530

    Fix: resolved index.js syntax error

--- mini-projects/07-todo-app-localstorage/src/index.js
+++ mini-projects/07-todo-app-localstorage/src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-<<<<<<< HEAD
-=======
-
->>>>>>> 00ba287 (Add 08 - Quote Generator (local version))
