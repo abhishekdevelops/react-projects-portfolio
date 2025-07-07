373e39bc6841c09d9a87c8a5c8bcf8fa3bccdf77
abhishek-web-design <abhishekvadnala10@gmail.com>
2025-07-07 11:07:41 +0530

    Updated Counter App UI and moved styles to App.css

--- mini-projects/01-counter-app/src/App.js
+++ mini-projects/01-counter-app/src/App.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
+import "./App.css";
 
-function App(){
+function App() {
+  const [count, setCount] = useState(0);
 
-  const[count,setCount]=useState(0);
+  let colorClass = "black";
+  if (count > 0) colorClass = "green";
+  else if (count < 0) colorClass = "red";
 
-  return(
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-    <h1>Counter App</h1>
-    <h2>{count}</h2>
-    <button onClick={()=>setCount(count+1)}>Increment</button>
-    <button onClick={()=>setCount(count-1)}>Decrement</button>
-    <button onClick={()=>setCount(0)}>Reset</button>
+  return (
+    <div className="container">
+      <h1>Counter App</h1>
+      <h2 className={colorClass}>{count}</h2>
+      <button className="btn increment" onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+      <button
+        className="btn decrement"
+        onClick={() => setCount(count > 0 ? count - 1 : 0)}
+      >
+        Decrement
+      </button>
+      <button className="btn reset" onClick={() => setCount(0)}>
+        Reset
+      </button>
     </div>
-  )
+  );
 }
+
 export default App;
